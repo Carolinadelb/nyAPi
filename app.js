b49ea8cdba1bfa6e5559f1feb591250ff3a0fbe6
Carolinadelb <carolinadelgadob@gmail.com>
2018-06-26 17:12:39 -0400

    ejercicio de lms XHR

---
+++ app.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('search-form');
+const searchField = document.getElementById('search-keyword');
+const responseContainer = document.getElementById('response-container');
+let searchForText;
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  responseContainer.innerHTML = '';
+  searchForText = searchField.value;
+  getNews();
+});
+
+function getNews() {
+  const articleRequest = new XMLHttpRequest();
+  articleRequest.open('GET', 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=5e061d8f967648cda4c0c1ce2690e569');
+  articleRequest.onload = addNews;
+  articleRequest.onerror = handleError;
+  articleRequest.send();
+}
+function handleError(){
+  console.log('se ha presentado un error');
+}
+
+function addNews(){
+  const data = JSON.parse(this.responseText);
+  const response = data.response;
+ 
+
+let li = document.createElement('li');
+li.className = 'articleClass';
+li.innerText = snippet;
+responseContainer.appendChild(li);
+}
