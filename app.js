339265c996e248277bcd2175ffd1ada25fd906ba
Carolinadelb <carolinadelgadob@gmail.com>
2018-06-27 16:46:01 -0400

    con cosita terminada

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', function (e) {
 
 function getNews() {
   const articleRequest = new XMLHttpRequest();
-  articleRequest.open('GET', 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=5e061d8f967648cda4c0c1ce2690e569');
+  articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchForText}&api-key=5e061d8f967648cda4c0c1ce2690e569`);
   articleRequest.onload = addNews;
   articleRequest.onerror = handleError;
   articleRequest.send();
@@ -24,10 +24,16 @@ function handleError(){
 function addNews(){
   const data = JSON.parse(this.responseText);
   const response = data.response;
+  const article = data.response.docs[0];
+  const title = article.headline.main;
+  const snippet = article.snippet;
+
  
 
 let li = document.createElement('li');
 li.className = 'articleClass';
 li.innerText = snippet;
 responseContainer.appendChild(li);
+
+
 }
